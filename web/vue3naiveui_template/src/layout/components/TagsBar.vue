1b06f84c166d55321dc3258508f794d75aa891f3
LiZhengHao <1263212577@qq.com>
2023-12-27 09:05:41 +0800

    修改窗体大小检测是否需要滚动任务栏

--- web/vue3naiveui_template/src/layout/components/TagsBar.vue
+++ web/vue3naiveui_template/src/layout/components/TagsBar.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
-import {useRouteMenuStore} from "@/stores/routeMenuStore";
-import {computed, onMounted, ref, watch,h} from "vue";
+import { useRouteMenuStore } from "@/stores/routeMenuStore";
+import { computed, onMounted, ref, watch, h, onUnmounted } from "vue";
 import router from "@/router";
 import SvgIcon from "@/components/icons/SvgIcon.vue";
-import {NIcon} from "naive-ui";
+import { NIcon } from "naive-ui";
+import _ from "lodash";
 
 const routeMenuStore = useRouteMenuStore();
 const onClose = (item) => {
   routeMenuStore.removeOpenRoute(item.routePath)
 }
 const onChangeRoute = (item) => {
-  router.push({path: item.routePath})
+  router.push({ path: item.routePath })
 }
 
 const scrollbar = ref(null)
@@ -20,27 +21,34 @@ const data = ref({
   yPos: 0,
   showDropdown: false,
   menuOptions: [
-    {key: '刷新当前', label: '刷新当前',icon () {
+    {
+      key: '刷新当前', label: '刷新当前', icon() {
         return h(NIcon, null, {
-          default: () => h(SvgIcon, {name: 'ReloadOutlined'})
+          default: () => h(SvgIcon, { name: 'ReloadOutlined' })
         })
       }
     },
-    {key: '关闭当前', label: '关闭当前',icon () {
+    {
+      key: '关闭当前', label: '关闭当前', icon() {
         return h(NIcon, null, {
-          default: () => h(SvgIcon, {name: 'IosClose',scale:1.4})
+          default: () => h(SvgIcon, { name: 'IosClose', scale: 1.4 })
         })
-      }},
-    {key: '关闭其他', label: '关闭其他',icon () {
+      }
+    },
+    {
+      key: '关闭其他', label: '关闭其他', icon() {
         return h(NIcon, null, {
-          default: () => h(SvgIcon, {name: 'CloseCircleOutlined'})
+          default: () => h(SvgIcon, { name: 'CloseCircleOutlined' })
         })
-      }},
-    {key: '关闭所有', label: '关闭所有',icon () {
+      }
+    },
+    {
+      key: '关闭所有', label: '关闭所有', icon() {
         return h(NIcon, null, {
-          default: () => h(SvgIcon, {name: 'ExclamationOutlined'})
+          default: () => h(SvgIcon, { name: 'ExclamationOutlined' })
         })
-      }},
+      }
+    },
   ],
   enableScroll: false
 
@@ -64,7 +72,7 @@ watch(() => [...routeMenuStore.openedRouteInfo], () => {
 const handleSelect = (value) => {
   switch (value) {
     case '刷新当前':
-      router.push({path: '/reload'})
+      router.push({ path: '/reload' })
       break
     case '关闭当前':
       routeMenuStore.removeOpenRoute(router.currentRoute.value.path)
@@ -74,7 +82,7 @@ const handleSelect = (value) => {
       break
     case '关闭所有':
       routeMenuStore.removeAllOpenRoute(router.currentRoute.value.path)
-      router.push({path: '/'})
+      router.push({ path: '/' })
       break
   }
   data.value.showDropdown = false
@@ -82,9 +90,15 @@ const handleSelect = (value) => {
 
 }
 
+const updateScrollThrottle = _.throttle(() => updateScroll, 500)
 onMounted(() => {
+  window.addEventListener('resize', updateScrollThrottle)
   updateScroll()
 })
+onUnmounted(() => {
+  window.removeEventListener('resize', updateScrollThrottle)
+})
+
 const scrollPrev = () => {
   scrollbar.value.scrollLeft -= (scrollbar.value.scrollWidth / 10); // adjust scrolling speed
 }
@@ -113,10 +127,8 @@ const updateScroll = async () => {
       </div>
       <div class="scrollbar flex flex-items-center gap-2 overflow-x-hidden overflow-y-hidden" ref="scrollbar">
         <n-tag closable class="cursor-pointer bg-white h-8 p-2 flex-shrink-0"
-               v-for="item in  routeMenuStore.openedRouteInfo"
-               :type="router.currentRoute.value.path==item.routePath?'info':''"
-               @click="onChangeRoute(item)"
-               @close="onClose(item)" @contextmenu="handleContextMenu">
+          v-for="item in  routeMenuStore.openedRouteInfo" :type="router.currentRoute.value.path == item.routePath ? 'info' : ''"
+          @click="onChangeRoute(item)" @close="onClose(item)" @contextmenu="handleContextMenu">
           {{ item?.label }}
         </n-tag>
       </div>
@@ -128,31 +140,23 @@ const updateScroll = async () => {
     <div class="cursor-pointer" @mouseenter="handleContextMenu">
       <svg-icon name="ArrowDown12Regular"></svg-icon>
     </div>
-    <n-dropdown
-        placement="bottom-start"
-        trigger="manual"
-        :x="data.xPos"
-        :y="data.yPos"
-        :options="data.menuOptions"
-        :show="data.showDropdown"
-        :on-clickoutside="onClickoutside"
-        @select="handleSelect"
-    ></n-dropdown>
+    <n-dropdown placement="bottom-start" trigger="manual" :x="data.xPos" :y="data.yPos" :options="data.menuOptions"
+      :show="data.showDropdown" :on-clickoutside="onClickoutside" @select="handleSelect"></n-dropdown>
 
   </div>
-
-
 </template>
 
 <style scoped lang="less">
-.tags-bar-root-box{
+.tags-bar-root-box {
   display: flex;
   align-items: center;
   gap: 10px;
-  >div:nth-child(1){
+
+  >div:nth-child(1) {
     width: 98%;
   }
-  >div:nth-child(2){
+
+  >div:nth-child(2) {
     width: 2%;
   }
 }
