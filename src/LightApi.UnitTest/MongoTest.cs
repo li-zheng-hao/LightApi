49f963775dbb34fa09bfef29180d8df727445495
LiZhengHao <1263212577@qq.com>
2024-04-18 20:31:51 +0800

    fix: 修复Mongodb分页查询错误
    (cherry picked from commit b5bda363d8a76c6885264b64fa9b67623b287959)

---
+++ src/LightApi.UnitTest/MongoTest.cs
@@ -0,0 +1,28 @@
+﻿using LightApi.Mongo.Entities;
+using LightApi.Mongo.Repository;
+using LightApi.Mongo.UnitOfWork;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Entities;
+
+namespace LightApi.UnitTest;
+
+public class MongoTest
+{
+    [Fact]
+    public async Task PageQuery()
+    {
+        IServiceCollection serviceCollection = new ServiceCollection();
+        serviceCollection.AddScoped(typeof(MongoRepository<>));
+        serviceCollection.AddScoped(typeof(IMongoUnitOfWork),typeof(MongoUnitOfWork));
+        DB.InitAsync("TestDb").Wait();
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var repository = serviceProvider.GetService<MongoRepository<ModelTest>>();
+        await repository!.InsertAsync(new ModelTest() { Name = "hello" });
+        var res=await repository.GetPaginatedAsync(it => true);
+        Assert.NotNull(res);
+    }
+    internal class ModelTest:MongoEntity
+    {
+        public string Name { get; set; }
+    }
+}
