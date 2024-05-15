484c2649081675e3766dc4842cb4d19b8f9bc542
LiZhengHao <1263212577@qq.com>
2024-05-15 16:45:35 +0800

    util更新
    (cherry picked from commit d4e22599476c25612220c05725614197298b1b11)

---
+++ web/vue3naiveui_template/src/utils/dynamicjs.js
@@ -0,0 +1,29 @@
+
+const loadedKey=[]
+
+/**
+ * 动态加载外部js文件
+ * @param url 外部js文件的url
+ * @param key 用于标记是否已经加载过的唯一key
+ * @returns {Promise<unknown>}
+ */
+export function load(url,key) {
+    if (loadedKey.includes(key)) {
+        return;
+    }
+    loadedKey.push(key);
+    const script = document.createElement('script');
+    script.src = url;
+
+    return new Promise((resolve, reject) => {
+        script.onload = () => {
+            console.log('finish loading lib from ' + url);
+            resolve();
+        };
+        script.onerror = (error) => {
+            console.error('Error loading lib from ' + url,  error);
+            reject(error);
+        };
+        document.head.appendChild(script);
+    });
+}
