43a0a6f9a84a2effd8b546e26a186851a8077228
lizhenghao <1263212577@qq.com>
2025-07-30 13:47:07 +0800

    4.2.2 infra

--- src/LightApi.Infra/Autofac/AutofacModuleRegister.cs
+++ src/LightApi.Infra/Autofac/AutofacModuleRegister.cs
@@ -26,71 +26,68 @@ public class AutofacModuleRegister : Module
             .Where(it => _dllPrefixes.Any(df => it.FullName?.StartsWith(df) == true))
             .ToList();
 
-        Parallel.ForEach(
-            assemblies,
-            it =>
+        assemblies.ForEach(it =>
+        {
+            var exportedTypes = it.GetExportedTypes().ToArray();
+
+            var transientTypes = new List<Type>();
+            var scopedTypes = new List<Type>();
+            var singletonTypes = new List<Type>();
+
+            foreach (var type in exportedTypes)
             {
-                var exportedTypes = it.GetExportedTypes().ToArray();
-
-                var transientTypes = new List<Type>();
-                var scopedTypes = new List<Type>();
-                var singletonTypes = new List<Type>();
-
-                foreach (var type in exportedTypes)
+                if (type.IsClass)
                 {
-                    if (type.IsClass)
+                    if (type.IsAssignableTo(typeof(ITransientDependency)))
                     {
-                        if (type.IsAssignableTo(typeof(ITransientDependency)))
-                        {
-                            transientTypes.Add(type);
-                        }
-                        if (type.IsAssignableTo(typeof(IScopedDependency)))
-                        {
-                            scopedTypes.Add(type);
-                        }
-                        if (type.IsAssignableTo(typeof(ISingletonDependency)))
-                        {
-                            singletonTypes.Add(type);
-                        }
+                        transientTypes.Add(type);
+                    }
+                    if (type.IsAssignableTo(typeof(IScopedDependency)))
+                    {
+                        scopedTypes.Add(type);
+                    }
+                    if (type.IsAssignableTo(typeof(ISingletonDependency)))
+                    {
+                        singletonTypes.Add(type);
                     }
                 }
-
-                builder
-                    .RegisterTypes(transientTypes.ToArray())
-                    .AsSelf()
-                    .InstancePerDependency()
-                    .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
-
-                builder
-                    .RegisterTypes(transientTypes.ToArray())
-                    .AsImplementedInterfaces()
-                    .InstancePerDependency()
-                    .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
-
-                builder
-                    .RegisterTypes(scopedTypes.ToArray())
-                    .AsSelf()
-                    .InstancePerLifetimeScope()
-                    .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
-
-                builder
-                    .RegisterTypes(scopedTypes.ToArray())
-                    .AsImplementedInterfaces()
-                    .InstancePerLifetimeScope()
-                    .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
-
-                builder
-                    .RegisterTypes(singletonTypes.ToArray())
-                    .AsSelf()
-                    .SingleInstance()
-                    .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
-
-                builder
-                    .RegisterTypes(singletonTypes.ToArray())
-                    .AsImplementedInterfaces()
-                    .SingleInstance()
-                    .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
             }
-        );
+
+            builder
+                .RegisterTypes(transientTypes.ToArray())
+                .AsSelf()
+                .InstancePerDependency()
+                .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
+
+            builder
+                .RegisterTypes(transientTypes.ToArray())
+                .AsImplementedInterfaces()
+                .InstancePerDependency()
+                .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
+
+            builder
+                .RegisterTypes(scopedTypes.ToArray())
+                .AsSelf()
+                .InstancePerLifetimeScope()
+                .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
+
+            builder
+                .RegisterTypes(scopedTypes.ToArray())
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope()
+                .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
+
+            builder
+                .RegisterTypes(singletonTypes.ToArray())
+                .AsSelf()
+                .SingleInstance()
+                .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
+
+            builder
+                .RegisterTypes(singletonTypes.ToArray())
+                .AsImplementedInterfaces()
+                .SingleInstance()
+                .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
+        });
     }
 }
